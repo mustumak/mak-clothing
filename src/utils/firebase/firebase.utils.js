801d310450a6cf38c64bbc1ca7c745745721bc5f
unknown <you@example.com>
2025-06-08 22:50:11 +0530

    added signIn component using firebase

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,58 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithPopup, signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore,doc,getDoc,setDoc } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA8Qm9I8lkQ8AiH7w8oZDoQgOPSOFYoE7E",
+  authDomain: "mak-clothing-db-7258f.firebaseapp.com",
+  projectId: "mak-clothing-db-7258f",
+  storageBucket: "mak-clothing-db-7258f.firebasestorage.app",
+  messagingSenderId: "169830484978",
+  appId: "1:169830484978:web:050676b562ee591ae9f96b",
+  measurementId: "G-QF0WDJGMVF"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    // console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    
+    if(!userSnapshot.exists()){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try{
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        }catch(e){
+            console.log('Error :', e.message);
+        }
+    }
+
+    return userDocRef;
+}
