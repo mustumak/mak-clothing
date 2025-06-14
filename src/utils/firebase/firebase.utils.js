fff4320427eeee432ea54eb2d139b6e8ed491102
unknown <you@example.com>
2025-06-14 22:54:44 +0530

    authentication page and sign in component

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,7 +3,8 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, signInWithPopup, 
     signInWithRedirect, GoogleAuthProvider,
-    createUserWithEmailAndPassword } from "firebase/auth";
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword } from "firebase/auth";
 import { getFirestore,doc,getDoc,setDoc } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -66,4 +67,10 @@ export const creteAuthUserWithEmailAndPassword = async (email,password) => {
     if(!email || !password) return;
 
     return await createUserWithEmailAndPassword(auth, email, password);
+}
+
+export const signInAuthUserWithEmailAndPassword = async (email,password) => {
+    if(!email || !password) return;
+
+    return await signInWithEmailAndPassword(auth, email, password);
 }
