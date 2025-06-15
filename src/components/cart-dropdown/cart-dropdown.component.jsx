aac9995bde86cfc470402c66426911ff790384aa
unknown <you@example.com>
2025-06-15 23:33:50 +0530

    Checkout done

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,16 +3,23 @@ import { CartContext } from '../../contexts/cart.context';
 import './cart-dropdown.styles.scss';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
+import { useNavigate } from 'react-router';
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
+    const navigate = useNavigate();
+
+    const goToCheckouthandler = () => {
+        navigate('/checkout');
+    }
+
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
                 {cartItems.map((item) => (
                     <CartItem key={item.id} cartItem={item} />
                 ))}
-                <Button>GO TO CHECKOUT</Button>
+                <Button onClick={goToCheckouthandler}>GO TO CHECKOUT</Button>
             </div>
         </div>
     )
